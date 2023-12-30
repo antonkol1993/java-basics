8b02134cd9aca30b9cc021deb2a1a49a33118030
PC Anton Kolesnikov <antonkol1993@gmail.com>
2023-12-31 00:51:21 +0300

    adjusted test NextDayTest

--- src/test/java/next/day/NextDayTest.java
+++ src/test/java/next/day/NextDayTest.java
@@ -13,17 +13,17 @@ class NextDayTest {
     @ParameterizedTest
     @CsvSource(value =
             {
-                    "3.3.18",
-                    "29.2.12",
-                    "28.2.115",
-                    "31.12.11"
+                    "3.3.18   . 2.3.18",
+                    "1.3.12  . 29.2.12",
+                    "1.3.115 . 28.2.115",
+                    "1.1.12 . 31.12.11"
 
             }, delimiter = '.')
-    void nextDayTest(int day, int month, int year) {
+    void nextDayTest(int day, int month, int year, int dayToMethod, int monthToMethod, int yearToMethod) {
         NextDay nextDay = new NextDay(day, month, year);
-        int[] dateTest = nextDay.nextDate(day, month, year);
+        int[] dateTest = {nextDay.getDay(),nextDay.getMonth(), nextDay.getYear(),};
 
-        assertEquals(Arrays.toString(dateTest), Arrays.toString(nextDay.nextDate(day, month, year)));
+        assertEquals(Arrays.toString(dateTest), Arrays.toString(nextDay.nextDate(dayToMethod, monthToMethod, yearToMethod)));
         System.out.println(nextDay.getDay() + " " + nextDay.getMonth() + " " + nextDay.getYear());
     }
 
