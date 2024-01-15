b769c472118a4a9e778cdb67aae9ce03009cf9ba
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-01-16 00:17:28 +0300

    15.01.2024 Doing methods in the class "line"

--- src/main/java/point/by/anton/Line.java
+++ src/main/java/point/by/anton/Line.java
@@ -4,13 +4,8 @@ public class Line extends Point implements GeometryUtils {
 
     private Point firstPointOfLine;
     private Point secondPointOfLine;
-    private Point point;
+    private Point intersectionPoint;
 
-
-    private static double x1;
-    private static double y1;
-    private static double x2;
-    private static double y2;
 
 
 //    private Point secondPointOfLine;
@@ -21,17 +16,10 @@ public class Line extends Point implements GeometryUtils {
     }
 
     public Line(double x1, double y1, double x2, double y2) {
-        Line.x1 = x1;
-        Line.y1 = y1;
-        Line.x2 = x2;
-        Line.y2 = y2;
         firstPointOfLine = new Point(x1, y1);
         secondPointOfLine = new Point(x2, y2);
     }
 
-    public Line(double X, Double Y) {
-        point = new Point(X,Y);
-    }
 
     public boolean isPointOnTheLine(double X, double Y) {
         //       todo
@@ -49,59 +37,80 @@ public class Line extends Point implements GeometryUtils {
         return leftvalue == rightValue;
     }
 
-    public static Line canonicalEquationOfTheLine(Line line1, Line line2) {
+    public static Point canonicalEquationOfTheLine(Line line1, Line line2) {
         // todo
         //       получение уравнения прямой Y = kx + b ;
         //       k = (y2 — y1) / (x2 — x1)
         //       b = y1 — k * x1.
-        //        (Y1) = y1 + (y2 — y1) / (x2 — x1) * (X — x1)
-        //        (Y2) = y1 + (y2 — y1) / (x2 — x1) * (X — x1)
         //
         //
-                double zero = 0;
 
-
-
-        double k1 = (line1.secondPointOfLine.getY() - line1.firstPointOfLine.getY()) / (line1.secondPointOfLine.getX() - line1.firstPointOfLine.getX() );
+        double k1 = (line1.secondPointOfLine.getY() - line1.firstPointOfLine.getY()) / (line1.secondPointOfLine.getX() - line1.firstPointOfLine.getX());
         double b1 = (line1.firstPointOfLine.getY() - (k1 * line1.getX()));
-        System.out.println(k1 + "    !!!  " + b1);
+        System.out.println("k1= " + k1 + " !!! " + "b1= " + b1);
 
-        double k2 = (line2.secondPointOfLine.getY() - line2.firstPointOfLine.getY()) / (line2.secondPointOfLine.getX() - line2.firstPointOfLine.getX() );
+        double k2 = (line2.secondPointOfLine.getY() - line2.firstPointOfLine.getY()) / (line2.secondPointOfLine.getX() - line2.firstPointOfLine.getX());
         double b2 = (line2.firstPointOfLine.getY() - (k2 * line2.getX()));
-//        (k1 * x + b1) - (k2 * x + b2) = 0 ;
-//        k1*x - k2*x =  b2 -b1;
-        double x = (b2 -b1) / (k1-k2);
-        System.out.println(x);
+        System.out.println("k2= " + k2 + " !!! " + "b2= " + b2);
 
+
+        if (k1 == k2 && b1 != b2) {
+            return null;
+        } else if (k1 == k2 && b1 == b2) {
+            throw new RuntimeException("These Lines have an infinite number of intersection points");
+
+        }
+        double x = (b2 - b1) / (k1 - k2);
+        System.out.println();
+        System.out.println(k1 + "*x + " + b1 + "==" + k2 + "*x + " + b2);
         //       todo
         //        (k1*x + b1) - ( k2* x + b2 ) = 0
         //        (k1* x) - (k2*x) = b2 - b1
         //        x * ( k1 - k2 ) = b2 - b1
         //        x = (b2 - b 1) / ( k1 - k2)
-        double y = line1.firstPointOfLine.getY() + ((double) (line1.secondPointOfLine.getY() - line1.firstPointOfLine.getY()) /
-                (line1.secondPointOfLine.getX() - line1.firstPointOfLine.getX()) * (x - line1.firstPointOfLine.getX()));
+        double y = k2 * x + b2;
+        System.out.println(x);
         System.out.println(y);
 
-//        if (line1.secondPointOfLine.getX() - line1.firstPointOfLine.getX() == 0 ||
-//                line2.secondPointOfLine.getX() - line2.firstPointOfLine.getX() == 0) {
-//            throw new RuntimeException("These parameters can't be ZERO ");
-//        }
-//        double X = 1;
-//
-//
-//
-//        double Y1 = line1.firstPointOfLine.getY() + (line1.secondPointOfLine.getY() - line1.firstPointOfLine.getY()) /
-//                (line1.secondPointOfLine.getX() - line1.firstPointOfLine.getX()) * (X - line1.firstPointOfLine.getX());
-//        double Y2 = line2.firstPointOfLine.getY() + (line2.secondPointOfLine.getY() - line2.firstPointOfLine.getY()) /
-//                (line2.secondPointOfLine.getX() - line2.firstPointOfLine.getX()) * (X - line2.firstPointOfLine.getX());
-//
-//
-//        System.out.println(Y + "   !!!!  " + rightValue);
-//            if ( Y == rightValue ) {
-//               return Object pointCoordinate
-//            }
-        return line1 ;
+        return new Point(x, y);
     }
+
+//    public static boolean isThereAnIntersectionPoint(Line line1, Line line2) {
+//        if (canonicalEquationOfTheLine(line1,line2) == null || canonicalEquationOfTheLine(line1, line2) == RuntimeException)
+//        return true;
+//    }
+
+    public static boolean isThereAnIntersectionPoint2(Line line1, Line line2) {
+        double k1 = (line1.secondPointOfLine.getY() - line1.firstPointOfLine.getY()) / (line1.secondPointOfLine.getX() - line1.firstPointOfLine.getX());
+        double k2 = (line2.secondPointOfLine.getY() - line2.firstPointOfLine.getY()) / (line2.secondPointOfLine.getX() - line2.firstPointOfLine.getX());
+
+        return k1 == k2;
+    }
+
+    public static boolean linesArePerpendicular(Line line1, Line line2) {
+        double k1 = (line1.secondPointOfLine.getY() - line1.firstPointOfLine.getY()) / (line1.secondPointOfLine.getX() - line1.firstPointOfLine.getX());
+
+
+        double k2 = (line2.secondPointOfLine.getY() - line2.firstPointOfLine.getY()) / (line2.secondPointOfLine.getX() - line2.firstPointOfLine.getX());
+
+        boolean answer;
+        answer = k1 * k2 == -1;
+        return answer;
+    }
+
+    public static boolean linesAreParallel(Line line1, Line line2) {
+        double k1 = (line1.secondPointOfLine.getY() - line1.firstPointOfLine.getY()) / (line1.secondPointOfLine.getX() - line1.firstPointOfLine.getX());
+        double k2 = (line2.secondPointOfLine.getY() - line2.firstPointOfLine.getY()) / (line2.secondPointOfLine.getX() - line2.firstPointOfLine.getX());
+
+        boolean answer;
+        answer = k1 == k2 ;
+        return answer;
+    }
+
+
+
+
+
 
     public static void main(String[] args) {
         // todo
@@ -112,11 +121,11 @@ public class Line extends Point implements GeometryUtils {
         //        (Y2) = y1 + (y2 — y1) / (x2 — x1) * (X — x1)
         //
         //
-        Line line1 = new Line(3, 1, 2, 4);
-        Line line2 = new Line(5, 2, 4, 8);
-        Line.canonicalEquationOfTheLine(line1,line2);
+        Line line1 = new Line(-1, 4, 2, 8);
+        Line line2 = new Line(-1, 4, 2, 8);
+//        Line.canonicalEquationOfTheLine(line1, line2);
 
-
+        System.out.println(Line.isThereAnIntersectionPoint(line1,line2));
 
 
     }
