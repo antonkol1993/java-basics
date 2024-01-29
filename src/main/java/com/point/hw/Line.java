8b423f9e7c556eb3e882b7a89c9112b3fb9f6893
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-01-29 19:35:55 +0300

    29.01 added object properties and constructor in the class 'Line'

--- src/main/java/com/point/hw/Line.java
+++ src/main/java/com/point/hw/Line.java
@@ -1,4 +1,19 @@
 package com.point.hw;
 
 public class Line {
+
+    private Point startLine;
+    private Point endLine;
+
+    public Line(double x1,double y1,double x2,double y2) {
+        this.startLine = new Point(x1,y1);
+        this.endLine = new Point(x2,y2);
+    }
+
+    public Line(Point startLine, Point endLine) {
+        this.startLine = startLine;
+        this.endLine = endLine;
+    }
+
+
 }
