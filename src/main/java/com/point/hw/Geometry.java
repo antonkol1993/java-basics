d8f5c89c54f9fa569d80c4434bee15a28056cc72
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-01-30 21:58:12 +0300

    30.01 created private methods "K" and "B" adn validateLine

--- src/main/java/com/point/hw/Geometry.java
+++ src/main/java/com/point/hw/Geometry.java
@@ -1,13 +1,38 @@
 package com.point.hw;
 
-import point.by.anton.LineOld;
-
 public class Geometry {
 
+    private boolean validateLine(Line line) {
+        if (line.getStartLine().getX() == line.getEndLine().getX() &&
+                line.getStartLine().getY() == line.getEndLine().getY()) {
+            throw new RuntimeException("\"Invalidate the next one line or both lines\"");
+        }
+        return true;
+    }
+
+    private double K(Line line) {
+        // todo: определяем "k" в уравнении прямой
+        //       k = (y2 — y1) / (x2 — x1)
+        validateLine(line);
+
+        return (line.getEndLine().getY() - line.getStartLine().getY()) /
+                (line.getEndLine().getX() - line.getStartLine().getX());
+    }
+
+    private double B(Line line) {
+        // todo: определяем "b" в уравнении прямой
+        //       b = y1 — k * x1.
+        validateLine(line);
+        return (line.getStartLine().getY() - (K(line) * line.getStartLine().getX()));
+    }
 
     public boolean isParallel(Line line1, Line line2) {
-        return false;
+        validateLine(line1);
+        validateLine(line2);
+        return K(line1) == K(line2);
     }
+
+
 
     public boolean isCrossed(Line line1, Line line2) {
         return false;
