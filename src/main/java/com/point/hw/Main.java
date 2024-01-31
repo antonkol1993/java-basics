d212ba6538459357a1a4713a5c91224724d2d16f
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-01-31 23:27:39 +0300

    31.01 1.Added Main for the check.

--- src/main/java/com/point/hw/Main.java
+++ src/main/java/com/point/hw/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         Geometry geometry = new Geometry();
+        Line line1 = new Line(new Point(5, 5), new Point(4, 2));
+        Line line2 = new Line(new Point(-10, -6), new Point(-9, 5));
 
-        Point parallel = geometry.isParallel(null, null);
+        System.out.println(geometry.crossPoint(line2, line1));
+
+
+        Point point = new Point(-14.25, -52.75);
+        System.out.println(point.equals(geometry.crossPoint(line1,line2)));
+        System.out.println(point);
+
+//        Point parallel = geometry.isParallel(null, null);
     }
 }
