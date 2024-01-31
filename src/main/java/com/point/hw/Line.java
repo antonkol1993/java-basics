8ae746e936940a1a8fae2dfbecce0e691363389e
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-01-31 23:26:28 +0300

    31.01 1.Renamed properties (in Class 'Line') 2. Added Getter and Setter for those proberties

--- src/main/java/com/point/hw/Line.java
+++ src/main/java/com/point/hw/Line.java
@@ -2,18 +2,27 @@ package com.point.hw;
 
 public class Line {
 
-    private Point startLine;
-    private Point endLine;
+    private Point startPoint;
+    private Point endPoint;
 
     public Line(double x1,double y1,double x2,double y2) {
-        this.startLine = new Point(x1,y1);
-        this.endLine = new Point(x2,y2);
+        this.startPoint = new Point(x1,y1);
+        this.endPoint = new Point(x2,y2);
     }
 
-    public Line(Point startLine, Point endLine) {
-        this.startLine = startLine;
-        this.endLine = endLine;
+    public Line(Point startPoint, Point endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
     }
 
+
+
+    public Point getStartPoint() {
+        return startPoint;
+    }
+
+    public Point getEndPoint() {
+        return endPoint;
+    }
 
 }
