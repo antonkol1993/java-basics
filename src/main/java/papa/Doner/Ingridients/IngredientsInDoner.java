b1f29bb65bbbab59441830b2d0a58870a31f1404
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-02-04 19:59:08 +0300

    04.02 1. Added 2 Enums ( souces and ingridients )

---
+++ src/main/java/papa/Doner/Ingridients/IngredientsInDoner.java
@@ -0,0 +1,18 @@
+package papa.Doner.Ingridients;
+
+public enum IngredientsInDoner {
+
+    FLATBREAD ,   // лепешка
+    FRESH_CABBAGE, // капуста свежая
+    CHICKEN,
+    TOMATO,
+    PICKLED_CUCUMBER,  // маринованный огурец
+    FRESH_CUCUMBER, // огурец свежий
+    CHINESE_CABBAGE, // пекинская капуста
+    MARBLE_BIF, // мраморная говядина
+    CREAM_CHEESE, // сливочный сыр
+    COWBERRY, // бурсника
+
+
+
+}
---
+++ src/main/java/papa/Doner/Ingridients/Sauces.java
@@ -0,0 +1,10 @@
+package papa.Doner.Ingridients;
+
+public enum Sauces {
+    GARLIC_SAUCE, // чесночный соус
+    RED_SAUCE,   // красный соус
+    TASTY_SAUCE, // соус ТЕЙСТИ
+    BARBECUE_SAUCE, // соус барбеккю
+    COWBERRY_SAUCE // брусничный соус
+
+}
