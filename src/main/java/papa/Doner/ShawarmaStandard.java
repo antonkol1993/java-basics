1796a4b8e9691c406335974410f01689a8bdbae9
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-02-05 00:29:33 +0300

    04.02 1. Complete the Class ShawarmaStandard

---
+++ src/main/java/papa/Doner/ShawarmaStandard.java
@@ -0,0 +1,55 @@
+package papa.Doner;
+
+import papa.Doner.Ingridients.IngredientsInDoner;
+import papa.Doner.Ingridients.Sauces;
+
+public abstract class ShawarmaStandard extends ServingQuantity {
+    private final IngredientsInDoner flatbread;
+    private IngredientsInDoner chicken;
+    private Sauces garlicSauce;
+
+
+    private ServingQuantity servingQuantity;
+
+
+    public ShawarmaStandard() {
+        flatbread = IngredientsInDoner.FLATBREAD;
+        garlicSauce = Sauces.GARLIC_SAUCE;
+        chicken = IngredientsInDoner.CHICKEN;
+
+        servingQuantity = new ServingQuantity();
+
+        servingQuantity.setQuantityFlatbread(1);
+        servingQuantity.setQuantityGarlicSauce(1);
+        servingQuantity.setQuantityChicken(1);
+    }
+
+
+    public IngredientsInDoner getChicken() {
+        return chicken;
+    }
+
+    public void setChicken(IngredientsInDoner chicken) {
+        this.chicken = chicken;
+    }
+
+    public IngredientsInDoner getFlatbread() {
+        return flatbread;
+    }
+
+    public Sauces getGarlicSauce() {
+        return garlicSauce;
+    }
+
+    public void setGarlicSauce(Sauces garlicSauce) {
+        this.garlicSauce = garlicSauce;
+    }
+
+    public ServingQuantity getServingQuantity() {
+        return servingQuantity;
+    }
+
+    public void setServingQuantity(ServingQuantity servingQuantity) {
+        this.servingQuantity = servingQuantity;
+    }
+}
