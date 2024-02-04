2a46dc0bd4b17f6c965e33e7d9a23e6519d7519c
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-02-05 00:28:47 +0300

    04.02 1. Added Sauces (  4 first doners)

--- src/main/java/papa/Doner/Ingridients/Sauces.java
+++ src/main/java/papa/Doner/Ingridients/Sauces.java
@@ -1,65 +1,10 @@
 package papa.Doner.Ingridients;
 
 public enum Sauces {
-    GARLIC_SAUCE {
-        int quantity = 1;
-
-        public int getQuantity() {
-            return quantity;
-        }
-
-        public void setQuantity(int quantity) {
-            this.quantity = quantity;
-        }
-
-    }, // чесночный соус
-    RED_SAUCE {
-        int quantity = 1;
-
-        public int getQuantity() {
-            return quantity;
-        }
-
-        public void setQuantity(int quantity) {
-            this.quantity = quantity;
-        }
-
-    },   // красный соус
-    TASTY_SAUCE {
-        int quantity = 1;
-
-        public int getQuantity() {
-            return quantity;
-        }
-
-        public void setQuantity(int quantity) {
-            this.quantity = quantity;
-        }
-
-    }, // соус ТЕЙСТИ
-    BARBECUE_SAUCE {
-        int quantity = 1;
-
-        public int getQuantity() {
-            return quantity;
-        }
-
-        public void setQuantity(int quantity) {
-            this.quantity = quantity;
-        }
-
-    }, // соус барбеккю
-    COWBERRY_SAUCE {
-        int quantity = 1;
-
-        public int getQuantity() {
-            return quantity;
-        }
-
-        public void setQuantity(int quantity) {
-            this.quantity = quantity;
-        }
-
-    } // брусничный соус
+    GARLIC_SAUCE, // чесночный соус
+    RED_SAUCE,   // красный соус
+    TASTY_SAUCE, // соус ТЕЙСТИ
+    BARBECUE_SAUCE, // соус барбеккю
+    COWBERRY_SAUCE // брусничный соус
 
 }
