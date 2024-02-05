fbaf7fb14a0995d0af582fecea6d350052f35d0a
Work PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-02-05 16:24:20 +0300

    05.02 at work 1. Created Class TotalCost 2. Rename class Cost to CostIngredients 3. wrote the prices in TotalCost and CostIngredients

---
+++ src/main/java/papa/Doner/cost_and_weight/CostIngredients.java
@@ -0,0 +1,134 @@
+package papa.Doner.cost_and_weight;
+
+public class CostIngredients {
+    private double costFlatbread = 1;
+
+    private double costChicken = 2;
+    private double costMarbleBif= 3;
+    private double costFreshCabbage = 0.5;
+    private double costTomato = 1.2;
+    private double costPickledCucumber= 1.2;
+    private double costChineseCabbage= 1.2;
+
+    private double costCreamCheese= 1.2;
+    private double costCowberry= 1.2;
+
+    private double costGarlicSauce= 1.2;
+    private double costRedSauce= 1.2;
+    private double costTastySauce= 1.2;
+    private double costBarbequeSauce= 1.2;
+    private double costCowberrySauce= 1.2;
+
+
+    public double getCostFlatbread() {
+        return costFlatbread;
+    }
+
+    public void setCostFlatbread(double costFlatbread) {
+        this.costFlatbread = costFlatbread;
+    }
+
+    public double getCostFreshCabbage() {
+        return costFreshCabbage;
+    }
+
+    public void setCostFreshCabbage(double costFreshCabbage) {
+        this.costFreshCabbage = costFreshCabbage;
+    }
+
+    public double getCostChicken() {
+        return costChicken;
+    }
+
+    public void setCostChicken(double costChicken) {
+        this.costChicken = costChicken;
+    }
+
+    public double getCostTomato() {
+        return costTomato;
+    }
+
+    public void setCostTomato(double costTomato) {
+        this.costTomato = costTomato;
+    }
+
+    public double getCostPickledCucumber() {
+        return costPickledCucumber;
+    }
+
+    public void setCostPickledCucumber(double costPickledCucumber) {
+        this.costPickledCucumber = costPickledCucumber;
+    }
+
+    public double getCostChineseCabbage() {
+        return costChineseCabbage;
+    }
+
+    public void setCostChineseCabbage(double costChineseCabbage) {
+        this.costChineseCabbage = costChineseCabbage;
+    }
+
+    public double getCostMarbleBif() {
+        return costMarbleBif;
+    }
+
+    public void setCostMarbleBif(double costMarbleBif) {
+        this.costMarbleBif = costMarbleBif;
+    }
+
+    public double getCostCreamCheese() {
+        return costCreamCheese;
+    }
+
+    public void setCostCreamCheese(double costCreamCheese) {
+        this.costCreamCheese = costCreamCheese;
+    }
+
+    public double getCostCowberry() {
+        return costCowberry;
+    }
+
+    public void setCostCowberry(double costCowberry) {
+        this.costCowberry = costCowberry;
+    }
+
+    public double getCostGarlicSauce() {
+        return costGarlicSauce;
+    }
+
+    public void setCostGarlicSauce(double costGarlicSauce) {
+        this.costGarlicSauce = costGarlicSauce;
+    }
+
+    public double getCostRedSauce() {
+        return costRedSauce;
+    }
+
+    public void setCostRedSauce(double costRedSauce) {
+        this.costRedSauce = costRedSauce;
+    }
+
+    public double getCostTastySauce() {
+        return costTastySauce;
+    }
+
+    public void setCostTastySauce(double costTastySauce) {
+        this.costTastySauce = costTastySauce;
+    }
+
+    public double getCostBarbequeSauce() {
+        return costBarbequeSauce;
+    }
+
+    public void setCostBarbequeSauce(double costBarbequeSauce) {
+        this.costBarbequeSauce = costBarbequeSauce;
+    }
+
+    public double getCostCowberrySauce() {
+        return costCowberrySauce;
+    }
+
+    public void setCostCowberrySauce(double costCowberrySauce) {
+        this.costCowberrySauce = costCowberrySauce;
+    }
+}
---
+++ src/main/java/papa/Doner/cost_and_weight/TotalCost.java
@@ -0,0 +1,34 @@
+package papa.Doner.cost_and_weight;
+
+public class TotalCost extends CostIngredients {
+    private double donerBarbequeCost;
+    private double donerChickenCost;
+    private double donerRaffleTastyCost;
+
+
+
+
+    public double getDonerBarbequeCost() {
+        return donerBarbequeCost;
+    }
+
+    public void setDonerBarbequeCost(double donerBarbequeCost) {
+        this.donerBarbequeCost = donerBarbequeCost;
+    }
+
+    public double getDonerChickenCost() {
+        return donerChickenCost;
+    }
+
+    public void setDonerChickenCost(double donerChickenCost) {
+        this.donerChickenCost = donerChickenCost;
+    }
+
+    public double getDonerRaffleTastyCost() {
+        return donerRaffleTastyCost;
+    }
+
+    public void setDonerRaffleTastyCost(double donerRaffleTastyCost) {
+        this.donerRaffleTastyCost = donerRaffleTastyCost;
+    }
+}
