d03a1493e350c08e9dbc37353f0fe4f800279efc
Work PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-02-05 16:34:16 +0300

    05.02 at work 1. Created Class TotalWeight and  WeightOfIngredients 2. wrote the weight in TotalWeight and WeightOfIngredients

---
+++ src/main/java/papa/Doner/cost_and_weight/weight/WeightOfIngredients.java
@@ -0,0 +1,135 @@
+package papa.Doner.cost_and_weight.weight;
+
+public class WeightOfIngredients {
+
+    private int weightFlatbread = 100;
+
+    private int weightChicken = 100;
+    private int weightMarbleBif= 100;
+    private int weightFreshCabbage = 70;
+    private int weightTomato = 30;
+    private int weightPickledCucumber= 30;
+    private int weightChineseCabbage= 30;
+
+    private int weightCreamCheese= 30;
+    private int weightCowberry= 20;
+
+    private int weightGarlicSauce= 15;
+    private int weightRedSauce= 15;
+    private int weightTastySauce= 15;
+    private int weightBarbequeSauce= 15;
+    private int weightCowberrySauce= 15;
+
+
+    public int getWeightFlatbread() {
+        return weightFlatbread;
+    }
+
+    public void setWeightFlatbread(int weightFlatbread) {
+        this.weightFlatbread = weightFlatbread;
+    }
+
+    public int getWeightChicken() {
+        return weightChicken;
+    }
+
+    public void setWeightChicken(int weightChicken) {
+        this.weightChicken = weightChicken;
+    }
+
+    public int getWeightMarbleBif() {
+        return weightMarbleBif;
+    }
+
+    public void setWeightMarbleBif(int weightMarbleBif) {
+        this.weightMarbleBif = weightMarbleBif;
+    }
+
+    public int getWeightFreshCabbage() {
+        return weightFreshCabbage;
+    }
+
+    public void setWeightFreshCabbage(int weightFreshCabbage) {
+        this.weightFreshCabbage = weightFreshCabbage;
+    }
+
+    public int getWeightTomato() {
+        return weightTomato;
+    }
+
+    public void setWeightTomato(int weightTomato) {
+        this.weightTomato = weightTomato;
+    }
+
+    public int getWeightPickledCucumber() {
+        return weightPickledCucumber;
+    }
+
+    public void setWeightPickledCucumber(int weightPickledCucumber) {
+        this.weightPickledCucumber = weightPickledCucumber;
+    }
+
+    public int getWeightChineseCabbage() {
+        return weightChineseCabbage;
+    }
+
+    public void setWeightChineseCabbage(int weightChineseCabbage) {
+        this.weightChineseCabbage = weightChineseCabbage;
+    }
+
+    public int getWeightCreamCheese() {
+        return weightCreamCheese;
+    }
+
+    public void setWeightCreamCheese(int weightCreamCheese) {
+        this.weightCreamCheese = weightCreamCheese;
+    }
+
+    public int getWeightCowberry() {
+        return weightCowberry;
+    }
+
+    public void setWeightCowberry(int weightCowberry) {
+        this.weightCowberry = weightCowberry;
+    }
+
+    public int getWeightGarlicSauce() {
+        return weightGarlicSauce;
+    }
+
+    public void setWeightGarlicSauce(int weightGarlicSauce) {
+        this.weightGarlicSauce = weightGarlicSauce;
+    }
+
+    public int getWeightRedSauce() {
+        return weightRedSauce;
+    }
+
+    public void setWeightRedSauce(int weightRedSauce) {
+        this.weightRedSauce = weightRedSauce;
+    }
+
+    public int getWeightTastySauce() {
+        return weightTastySauce;
+    }
+
+    public void setWeightTastySauce(int weightTastySauce) {
+        this.weightTastySauce = weightTastySauce;
+    }
+
+    public int getWeightBarbequeSauce() {
+        return weightBarbequeSauce;
+    }
+
+    public void setWeightBarbequeSauce(int weightBarbequeSauce) {
+        this.weightBarbequeSauce = weightBarbequeSauce;
+    }
+
+    public int getWeightCowberrySauce() {
+        return weightCowberrySauce;
+    }
+
+    public void setWeightCowberrySauce(int weightCowberrySauce) {
+        this.weightCowberrySauce = weightCowberrySauce;
+    }
+}
