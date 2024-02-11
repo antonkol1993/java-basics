eb89a4a0f5b39a1c945eb11d0ac2384e5187c3ee
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-02-11 14:54:37 +0300

    11.02 1. Completed "Filters" Class ( all methods for filter )

--- src/main/java/newDoner/papa/Filters.java
+++ src/main/java/newDoner/papa/Filters.java
@@ -1,9 +1,10 @@
 package newDoner.papa;
 
 import newDoner.papa.ingridients.Ingredients;
+import newDoner.papa.ingridients.Sauces;
 
 public class Filters {
-    public static boolean isWithIngredient(Object object, Ingredients ingredients) {
+    public static boolean ingredientFilter(Doner object, Ingredients ingredients) {
         if (object instanceof Doner obj1) {
 
             for (int i = 0; i < obj1.getIngredients().size(); i++) {
@@ -15,4 +16,35 @@ public class Filters {
         }
         return false;
     }
+
+    public static boolean saucesFilter(Doner object, Sauces sauces) {
+        if (object instanceof Doner obj1) {
+            for (int i = 0; i < obj1.getSauces().size(); i++) {
+
+                if (obj1.getSauces().get(i) == sauces) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean weightFilter(Doner object, int weight) {
+        return object.getWeight() == weight;
+    }
+    public static boolean priceFilter(Doner object, double price) {
+        return object.getPrice() == price;
+    }
+
+    public static boolean nameFilter(Doner object, String name) {
+        return name.equalsIgnoreCase(object.getName());
+    }
+    public static boolean urlFilter(Doner object, String imageURL) {
+        return imageURL.equalsIgnoreCase(object.getImageURL());
+    }
+
+
+
+
+
 }
