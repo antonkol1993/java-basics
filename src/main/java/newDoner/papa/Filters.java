5a47fb4281b9221bf941b237e94cfd9f6cc989fb
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-02-11 15:36:42 +0300

    11.02 1. Completed "Filters" Class with method two ingredients

--- src/main/java/newDoner/papa/Filters.java
+++ src/main/java/newDoner/papa/Filters.java
@@ -4,13 +4,32 @@ import newDoner.papa.ingridients.Ingredients;
 import newDoner.papa.ingridients.Sauces;
 
 public class Filters {
-    public static boolean ingredientFilter(Doner object, Ingredients ingredients) {
+
+
+    public static boolean ingredientFilter(Doner object, Ingredients ingredient) {
         if (object instanceof Doner obj1) {
 
             for (int i = 0; i < obj1.getIngredients().size(); i++) {
 
-                if (obj1.getIngredients().get(i) == ingredients) {
+                if (obj1.getIngredients().get(i) == ingredient) {
                     return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean ingredientFilters2(Doner object, Ingredients ingredients1, Ingredients ingredients2) {
+        if (object instanceof Doner obj1) {
+
+
+            for (int i = 0; i < obj1.getIngredients().size(); i++) {
+                if (object.getIngredients().get(i) == ingredients1) {
+                    for (int j = 0; j < obj1.getIngredients().size(); j++) {
+                        if (object.getIngredients().get(j) == ingredients2) {
+                            return true;
+                        }
+                    }
                 }
             }
         }
@@ -32,6 +51,7 @@ public class Filters {
     public static boolean weightFilter(Doner object, int weight) {
         return object.getWeight() == weight;
     }
+
     public static boolean priceFilter(Doner object, double price) {
         return object.getPrice() == price;
     }
@@ -39,12 +59,10 @@ public class Filters {
     public static boolean nameFilter(Doner object, String name) {
         return name.equalsIgnoreCase(object.getName());
     }
+
     public static boolean urlFilter(Doner object, String imageURL) {
         return imageURL.equalsIgnoreCase(object.getImageURL());
     }
-
-
-
 
 
 }
