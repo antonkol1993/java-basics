fe8800b9dba237897b8696cc05fb644aab9e0336
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-03-04 21:45:54 +0300

    04.03 1. The solution of the problem

--- src/main/java/collections/map/ChatUtils.java
+++ src/main/java/collections/map/ChatUtils.java
@@ -1,49 +1,82 @@
 package collections.map;
 
+
 public class ChatUtils {
 
+    private static int stringsQuantity() {
+        dialogSplit();
+        return dialogSplit().length;
+    }
+
+    private static String[] dialogSplit() {
+        return ChatIssue.getSomeDialog().split("\\n");
+    }
 
 
-     public static String[] nameList() {
-          String[] words = ChatIssue.getSomeDialog().split("\\n");
-          String [] nameList = new String[words.length];
-         for (int i = 0; i < nameList.length; i++) {
-             String word = words[i];
-             String name = word.split(":")[0];
-
-             nameList[i] = name;
-         }
-          return nameList ;
-     }
+    public static String[] nameList() {
+        dialogSplit();
+        String[] nameList = new String[dialogSplit().length];
+        for (int i = 0; i < nameList.length; i++) {
+            String name = dialogSplit()[i].split(":")[0];
+            nameList[i] = name;
+        }
+        return nameList;
+    }
 
 
-     public static int [] quantity() {
-         String[] words = ChatIssue.getSomeDialog().split("\\n");
-         int [] quantityWordsInString = new int [words.length];
-         for (int j = 0; j<words.length; j++) {
+    public static Integer[] wordsQuantity() {
+        dialogSplit();
+        Integer[] quantityWordsInString = new Integer[dialogSplit().length];
+        for (int j = 0; j < dialogSplit().length; j++) {
 
-             String word = words[j];
-             String[] subString = word.split(" ");
-             int quantityWords = subString.length - 1;
+            String[] subString = dialogSplit()[j].split(" ");
+            int quantityWords = subString.length - 1;
 
-             quantityWordsInString[j] = quantityWords;
-         }
-         return quantityWordsInString;
-     }
-
-//     String[] words = ChatIssue.someDialog.split("\\n");
+            quantityWordsInString[j] = quantityWords;
+        }
+        return quantityWordsInString;
+    }
 
 
 
 
+
+
+//    public static List<String> getTopChatter(String dialog, int users) {
+//        List<String> adsadsads = null;
+//
+//        for (int i = 0; i < stringsQuantity(); i++) {
+//            Integer integrewer = 0;
+//            for (int j = stringsQuantity() - 1; j >= 0; j--) {
+//
+//
+//                    if (Objects.equals(nameList()[i], nameList()[j]) && j > i) {
+//                        integrewer += wordsQuantity()[j] + wordsQuantity()[i];
+//
+//                        wordsQuantity()[j] = 0;
+//                        nameList()[j] = "";
+//
+//                    }
+//
+//            }
+//            if (nameList()[i] != "" && wordsQuantity()[i] != 0) {
+//                adsadsads = List.of(nameList()[i], integrewer.toString()
+//                );
+//            }
+//
+//        }
+//
+//        return adsadsads;
+//    }
+
+
+    //     String[] words = ChatIssue.someDialog.split("\\n");
 
 
 //          System.out.println(nameList[j]);
 //          System.out.println(quantityWordsInString[j]);
 
-     }
 
 
-
-
+}
 //}
--- src/main/java/collections/map/Main.java
+++ src/main/java/collections/map/Main.java
@@ -1,6 +1,7 @@
 package collections.map;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
@@ -8,6 +9,17 @@ public class Main {
 
             System.out.println(Arrays.toString(ChatUtils.nameList()));
 
-        System.out.println(Arrays.toString(ChatUtils.quantity()));
+        System.out.println(Arrays.toString(ChatUtils.wordsQuantity()));
+
+        Integer [] arr;
+        arr = ChatUtils.wordsQuantity();
+        Arrays.sort(arr,Collections.reverseOrder());;
+        Arrays.sort(ChatUtils.wordsQuantity());
+
+            System.out.print(Arrays.toString(arr) + " ");
+
+
+
+//        System.out.println(ChatUtils.getTopChatter("asddas", 45));
     }
 }
