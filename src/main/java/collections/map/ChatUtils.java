760886c9a2480c8583c4f996c806c994ba1ff7a1
WorkComputer <antonkol1993@gmail.com>
2024-03-04 16:54:43 +0300

    04/03 The Solution from my work computer

--- src/main/java/collections/map/ChatUtils.java
+++ src/main/java/collections/map/ChatUtils.java
@@ -4,12 +4,46 @@ public class ChatUtils {
 
 
 
+     public static String[] nameList() {
+          String[] words = ChatIssue.getSomeDialog().split("\\n");
+          String [] nameList = new String[words.length];
+         for (int i = 0; i < nameList.length; i++) {
+             String word = words[i];
+             String name = word.split(":")[0];
+
+             nameList[i] = name;
+         }
+          return nameList ;
+     }
+
+
+     public static int [] quantity() {
+         String[] words = ChatIssue.getSomeDialog().split("\\n");
+         int [] quantityWordsInString = new int [words.length];
+         for (int j = 0; j<words.length; j++) {
+
+             String word = words[j];
+             String[] subString = word.split(" ");
+             int quantityWords = subString.length - 1;
+
+             quantityWordsInString[j] = quantityWords;
+         }
+         return quantityWordsInString;
+     }
+
+//     String[] words = ChatIssue.someDialog.split("\\n");
 
 
 
-     public static String adsasd = "null";
+
+
+
+//          System.out.println(nameList[j]);
+//          System.out.println(quantityWordsInString[j]);
+
+     }
 
 
 
 
-}
+//}
---
+++ src/main/java/collections/map/Main.java
@@ -0,0 +1,13 @@
+package collections.map;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            System.out.println(Arrays.toString(ChatUtils.nameList()));
+
+        System.out.println(Arrays.toString(ChatUtils.quantity()));
+    }
+}
