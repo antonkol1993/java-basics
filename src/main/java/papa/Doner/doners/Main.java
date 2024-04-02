b065e6246a539d9e3d1fc7e6366c39ddba5f1c1b
PC Anton Kolesnikov <antonkol1993@gmail.com>
2024-04-02 22:06:40 +0300

    02.04 transactions

---
+++ src/main/java/papa/Doner/doners/Main.java
@@ -0,0 +1,22 @@
+package papa.Doner.doners;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Doner donerBBQ = new Doner("Doner Raffle BBQ",
+//                List.of(FLATBREAD, TOMATO, PICKLED_CUCUMBER, CHINESE_CABBAGE, MARBLE_BIF),
+//                List.of(BARBECUE_SAUCE, GARLIC_SAUCE),
+//                400,
+//                9.90,
+//                "URL");
+//
+//
+//        Doner donerSwiss = new Doner("Doner Swiss",
+//                List.of(FLATBREAD, TOMATO, PICKLED_CUCUMBER, CHINESE_CABBAGE, MARBLE_BIF),
+//                List.of(BARBECUE_SAUCE, GARLIC_SAUCE),
+//                400,
+//                9.90,
+//                "URL2", true);
+
+    }
+}
