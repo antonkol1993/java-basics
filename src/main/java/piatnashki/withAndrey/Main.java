99c44004ef2559bc35f75cfd5142728cedd93925
Anton from Work <kOlesnik-0v>
2024-04-17 16:56:18 +0300

    17.04 doing

--- src/main/java/piatnashki/withAndrey/Main.java
+++ src/main/java/piatnashki/withAndrey/Main.java
@@ -19,7 +19,12 @@ public class Main {
             for (;;) {
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("enter the number");
-                int numb = scanner.nextInt();
+
+                    int numb = scanner.nextInt();
+
+                if (numb < 1 || numb > 15 ) {
+                    System.out.println("Please enter the correct number (from 1 to 15 )");
+                }
 
                 GameBoard newBoard = gameBoardService.startMove(gameBoard, numb);
                 System.out.println(newBoard);
